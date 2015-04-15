b76061dd6a0fb49a104e5dfab22e906f1c3619c5
Matteo Calosci <matteo1789@live.it>
2015-04-15 23:24:37 +0200

    FIRST

--- main.c
+++ main.c
@@ -1,26 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-// Definisci le costanti
+#define N 5
+#define MAX_STRLEN 30
 
 // Definisci il tipo di dato struct s_studente
+struct s_studente {
+    char cognome [MAX_STRLEN];
+    char nome [MAX_STRLEN];
+    int eta;
+    char classe[MAX_STRLEN];
+};
 
 // Definisci studente come struct s_studente
+struct s_studente studente[N];
 
 // Dichiara il prototipo della funzione di ordinamento
-
+void Ordina_Record(struct s_studente v[]);
 
 int main(int argc, char** argv) {
     // Dichiara le variabili locali
+    int i;
     
     // Codice per l'inserimento, da parte dell'utente, dei dati
+    for(i = 0; i < N; i++) {
+        printf("Inserisci il nome dello studente %d: ", i);
+        scanf("%s", studente[i].nome);
+        printf("Inserisci il cognome dello studente %d: ", i);
+        scanf("%s", studente[i].cognome);
+        printf("Inserisci la classe dello studente %d: ", i);
+        scanf("%s", studente[i].classe);
+        printf("Inserisci l'eta' dello studente %d: ", i);
+        scanf("&d", &(studente[i].eta));
+        
+    }
     
     // Ordina l'elenco (con funzione)
+    Ordina_Record(studente);
     
     // Codice per la stampa dei dati (uno studente per riga)
-    
+    for(i = 0; i < N; i++) {
+    printf("%s; %s; %s; %s;", studente[i].nome, studente[i].cognome, studente[i].eta, studente[i].classe);
+    }
     return (EXIT_SUCCESS);
 }
 
 // Definisci la funzione di ordinamento
+void Ordina_Record(struct s_studente v[]) {
+    struct s_studente aiuto;
+    int x,i,pos_min;
+    
+    for(x = 0; x < (N - 1); x++) {        
+    pos_min = x;
+      for(i = x + 1; i < N; i++) {          
+            if(strcmp (v[i].cognome,v[pos_min].cognome) < 0) {
+            pos_min = i;
+            }
+      
+                    if(x != pos_min) {           
+                    aiuto = v[pos_min];
+                    v[pos_min] = v[x];
+                    v[x] = aiuto;
+                    }
+        }
+    }
+}
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=ese031-elencostudentiordinato
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/ese031-elencostudentiordinato
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=ese031-elencostudentiordinato.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/ese031-elencostudentiordinato.tar
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Debug=record_ord
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/record_ord
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Debug=recordord.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/recordord.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
-CND_ARTIFACT_NAME_Release=ese031-elencostudentiordinato
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/ese031-elencostudentiordinato
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
-CND_PACKAGE_NAME_Release=ese031-elencostudentiordinato.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/ese031-elencostudentiordinato.tar
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Release=record_ord
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/record_ord
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Release=recordord.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/recordord.tar
 #
 # include compiler specific variables
 #
